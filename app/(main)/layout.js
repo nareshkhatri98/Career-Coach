09bae984b979b9dcb3e58c8d075edbfe8654e6b1
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-03-31 20:16:08 +0545

    create routes

---
+++ app/(main)/layout.js
@@ -0,0 +1,9 @@
+
+const MainLayout = ({children}) => {
+  return (
+    // Redirect user after onboarding..
+    <div className="container mx-auto mt-24  mb-20">{children}</div>
+  )
+}
+
+export default MainLayout
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
 
+import { checkUser } from "@/lib/inngest/checkUser";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -21,41 +23,42 @@ const Header = () => {
                 <span className="hidden md:block">Industry Insights </span>
               </Button>
             </Link>
-         
 
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button>
-                <StarsIcon className="h-4 w-4" />
-                <span className="hidden md:block">Growth Tools </span>
-                <ChevronDown className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href={'/resume'} className="flex items-center gap-2">
 
-                  <FileText className="h-4 w-4" />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarsIcon className="h-4 w-4" />
+                  <span className="hidden md:block">Growth Tools </span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
 
-                  <span >Build Resume </span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href={'/cover-letter'} className="flex items-center gap-2">
-                  <PenBox className="h-4 w-4" />
-                  <span>Cover letter</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href={'/interview-prepare'} className=" flex items-center gap-2">
-                  <GraduationCap className="h-4 w-4" />
-                  Interview Prepare
-                </Link>
-              </DropdownMenuItem>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={'/resume'} className="flex items-center gap-2">
 
-            </DropdownMenuContent>
-          </DropdownMenu>
-          </SignedIn> 
+                    <FileText className="h-4 w-4" />
+
+                    <span >Build Resume </span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={'/cover-letter'} className="flex items-center gap-2">
+                    <PenBox className="h-4 w-4" />
+                    <span>Cover letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={'/interview-prepare'} className=" flex items-center gap-2">
+                    <GraduationCap className="h-4 w-4" />
+                    Interview Prepare
+                  </Link>
+                </DropdownMenuItem>
+
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SignedIn>
 
           {/* if user is not login */}
           <SignedOut>
@@ -66,15 +69,15 @@ const Header = () => {
           {/* if user in login then show profile */}
           <SignedIn>
             <UserButton appearance={{
-              elements:{
-                avatarBox:"w-10 h-10",
-                userButtonPopoverCard:"shadow-xl",
-                userPreviewMainIdentifier:"font-semibold",
+              elements: {
+                avatarBox: "w-10 h-10",
+                userButtonPopoverCard: "shadow-xl",
+                userPreviewMainIdentifier: "font-semibold",
               },
             }}
-            afterSignOutUrl="/"
+              afterSignOutUrl="/"
             />
-          
+
           </SignedIn>
 
 
