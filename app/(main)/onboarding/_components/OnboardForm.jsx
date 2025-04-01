c4dab09a955da5ac927085cf7c27d6845b3be0b3
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-04-01 21:38:59 +0545

    enhance onboarding form with industry selection, experience input, and bio section

--- app/(main)/onboarding/_components/OnboardForm.jsx
+++ app/(main)/onboarding/_components/OnboardForm.jsx
@@ -1,35 +1,181 @@
 "use client";
 
 import { onboardingSchema } from "@/app/lib/schema";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 const OnboardForm = ({ industries }) => {
-    const [selectedIndustry, setSelectedIndustry] = useState(null);
-    const router = useRouter()
+  const [selectedIndustry, setSelectedIndustry] = useState(null);
+  const router = useRouter();
 
-    const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm({
-        resolver: zodResolver(onboardingSchema),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(onboardingSchema),
+  });
+  const watchIndustry = watch("industry");
 
-    })
-    return (
-        <div className="flex items-center justify-center bg-background">
-            <Card className={"w-full max-w-lg mt-10 mx-20"}>
-                <CardHeader>
-                    <CardTitle>Complete your profile</CardTitle>
-                    <CardDescription>Select your industry to get personalized career  insights and recommendations</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                
-            </Card>
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="flex items-center justify-center bg-background">
+      <Card className={"w-full max-w-lg mt-10 mx-20"}>
+        <CardHeader>
+          <CardTitle className={"gradient gradient-title text-4xl"}>
+            Complete your profile
+          </CardTitle>
+          <CardDescription>
+            Select your industry to get personalized career insights and
+            recommendations
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            {/* for industry */}
+            <div className="space-y-2">
+              <Label htmlFor="industry">Industry</Label>
+              <Select
+                onValueChange={(value) => {
+                  setValue("industry", value);
+                  setSelectedIndustry(
+                    industries.find((ind) => ind.id === value)
+                  );
+                  setValue("subIndustry", "");
+                }}
+              >
+                <SelectTrigger id="industry" className={"w-full "}>
+                  <SelectValue placeholder="Select an industry" />
+                </SelectTrigger>
+                <SelectContent>
+                  {industries.map((ind) => {
+                    return (
+                      <SelectItem key={ind.id} value={ind.id}>
+                        {ind.name}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectContent>
+              </Select>
+              {errors.industry && (
+                <p className="text-sm text-red-500">
+                  {errors.industry.message}
+                </p>
+              )}
+            </div>
 
-        </div>
-    )
-}
+            {watchIndustry && (
+              // for subIndustry
+              <div className="space-y-2">
+                <Label htmlFor="subIndustry">Specializations</Label>
+                <Select
+                  onValueChange={(value) => {
+                    setValue("industry", value);
+                  }}
+                >
+                  <SelectTrigger id="subIndustry" className={"w-full "}>
+                    <SelectValue placeholder="Select subIndustry" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {selectedIndustry?.subIndustries.map((ind) => {
+                      return (
+                        <SelectItem key={ind} value={ind}>
+                          {ind}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+                {errors.subIndustry && (
+                  <p className="text-sm text-red-500">
+                    {errors.subIndustry.message}
+                  </p>
+                )}
+              </div>
+            )}
 
-export default OnboardForm
+            {/* for experience */}
+            <div className="space-y-2">
+              <Label htmlFor="experience">Years of Experience</Label>
+              <Input
+                placeholder="Enter years of experience"
+                id="experience"
+                type={"number"}
+                min="0"
+                max="50"
+                {...register("experience")}
+              />
+
+              {errors.experience && (
+                <p className="text-sm text-red-500">
+                  {errors.experience.message}
+                </p>
+              )}
+            </div>
+
+            {/* skills */}
+            <div className="space-y-2">
+              <Label htmlFor="skills">Skills</Label>
+              <Input
+                placeholder="e.g., python, java, typeScript, project management"
+                id="skills"
+                {...register("skills")}
+              />
+              <p className="text-sm text-muted-foreground">
+                Separate multiple skills with commas
+              </p>
+
+              {errors.skills && (
+                <p className="text-sm text-red-500">{errors.skills.message}</p>
+              )}
+            </div>
+
+            {/* for bio */}
+
+            <div className="space-y-2">
+              <Label htmlFor="bio">Personal Bio</Label>
+              <Textarea
+                placeholder="Tell us your your professional background..."
+                id="bio"
+                className={"h-32"}
+                {...register("bio")}
+              />
+            
+
+              {errors.bio && (
+                <p className="text-sm text-red-500">{errors.bio.message}</p>
+              )}
+            </div>
+            <Button type="submit" className={"w-full"}>Complete Profile</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default OnboardForm;
