4f7ca27e69508bbc88f96f5ac6916678a6d54afd
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-04-01 20:33:30 +0545

    design schema for obboarding

--- app/(main)/onboarding/_components/OnboardForm.jsx
+++ app/(main)/onboarding/_components/OnboardForm.jsx
@@ -1,8 +1,35 @@
+"use client";
 
-const OnboardForm = ({industries}) => {
-  return (
-    <div>OnboardForm</div>
-  )
+import { onboardingSchema } from "@/app/lib/schema";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+const OnboardForm = ({ industries }) => {
+    const [selectedIndustry, setSelectedIndustry] = useState(null);
+    const router = useRouter()
+
+    const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm({
+        resolver: zodResolver(onboardingSchema),
+
+    })
+    return (
+        <div className="flex items-center justify-center bg-background">
+            <Card className={"w-full max-w-lg mt-10 mx-20"}>
+                <CardHeader>
+                    <CardTitle>Complete your profile</CardTitle>
+                    <CardDescription>Select your industry to get personalized career  insights and recommendations</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                
+            </Card>
+
+        </div>
+    )
 }
 
 export default OnboardForm
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -3,18 +3,17 @@ import { industries } from "@/data/industries";
 import { redirect } from "next/navigation";
 import OnboardForm from "./_components/OnboardForm";
 
-const OnBoardingPage = async() => {
+const OnBoardingPage = async () => {
   // check if user is already on board or not
 
-  const { isOnboarded} = await getUserOnboardingStatus();
-  
-  if(isOnboarded){
+  const { isOnboarded } = await getUserOnboardingStatus();
+  if (isOnboarded) {
     redirect("/dashboard");
   }
 
   return (
     <main>
-      <OnboardForm industries ={industries}/>
+      <OnboardForm industries={industries} />
     </main>
   )
 }
